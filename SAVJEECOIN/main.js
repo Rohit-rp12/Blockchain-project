4bffe48bf74fa9bd9e5dfe41c977d7c4196a095d
Rohit Panjwani <112685068+Rohit-rp12@users.noreply.github.com>
2024-08-22 23:42:54 +0530

    check

--- SAVJEECOIN/main.js
+++ SAVJEECOIN/main.js
@@ -1,4 +1,4 @@
-const SHA256 = require('crypto-js/sha256');    // import sha256 function
+const SHA256 = require('crypto-js/sha256');    // import sha256 function  
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
